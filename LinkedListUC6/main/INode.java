fbb3cd760e41df9c9d50f730f9d4f2a609bfbe32
himanshunete <himanshuneteh@gmail.com>
2021-01-27 10:50:55 +0530

    ADDED: functionality to delete last element in linked list

---
+++ LinkedListUC6/main/INode.java
@@ -0,0 +1,11 @@
+package javapractiseuc6;
+
+public interface INode<K> {
+	K getKey() ;
+	void setKey(K key);
+	   
+	INode getNext();
+	void setNext(INode next);
+
+}
+
---
+++ LinkedListUC6/main/MyLinkedList.java
@@ -0,0 +1,78 @@
+package javapractiseuc6;
+
+public class MyLinkedList {
+	   public INode head;
+	   public INode tail;
+		
+		public MyLinkedList() {
+			this.head = null ;
+			this.tail = null ; 		
+		}
+		
+		public void add(INode newNode) {
+			if (this.tail == null) {
+				this.tail = newNode ;
+			}
+			
+			if (this.head == null) {
+				this.head = newNode ;
+			}else {
+				INode tempNode = this.head;
+				this.head = newNode ;
+				this.head.setNext(tempNode);
+			}
+			
+			
+		}
+		
+		public void append(INode newNode) {
+			if (this.head == null) {
+				this.head = newNode ;
+			}
+			if (this.tail == null) {
+				this.tail = newNode ;
+			}else {
+				INode tempNode = this.tail;
+				this.tail = newNode ;
+				tempNode.setNext(newNode);
+			}
+			
+		}
+		
+		public void insert(INode myNode, INode newNode) {
+			this.head.setNext(myNode);
+			myNode.setNext(newNode);
+			
+			
+		}
+		
+		public INode pop() {
+			INode  tempNode = this.head;
+			this.head = head.getNext();			
+			return tempNode;
+		}
+		
+		public INode popLast() {
+			INode tempNode = head;
+			while (!tempNode.getNext().equals(tail)){
+				tempNode = tempNode.getNext();
+			}
+			this.tail = tempNode;
+			tempNode = tail.getNext();
+			tempNode = null;
+			return tempNode ;
+		}
+		
+		public void printMyNodes() {
+			StringBuffer myNodes = new StringBuffer("My Nodes: ");
+	        INode tempNode = head ;
+	        while (tempNode.getNext() !=null) {
+	        	myNodes.append(tempNode.getKey());
+	        	if(!tempNode.equals(tail)) myNodes.append("->");
+	            tempNode = tempNode.getNext();
+	            
+	        }
+	        myNodes.append(tempNode.getKey());
+	        System.out.println(myNodes);
+		}
+	}
